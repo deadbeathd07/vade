f2820451bbed4a6a6f5b7167f667b1c9ff7b1d31
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-06 17:48:37 +0300

    vuetify and storage are imported and connected to the main file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './vuetify';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router).use(vuetify);
 
-app.mount('#app')
+app.mount('#app');
