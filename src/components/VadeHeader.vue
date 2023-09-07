5d8671659ccd6690c9c1aab0c42ce15411e1ff1e
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-07 13:38:12 +0300

    nav component has been imported and plugged

--- src/components/VadeHeader.vue
+++ src/components/VadeHeader.vue
@@ -10,37 +10,15 @@
 			<img src="../assets/logo-1.svg" alt="logo" />
 		</template>
 		<template v-slot:append>
-			<v-list
-				class="d-flex opacity-40"
-				:base-color="palette['primary-nav-text']"
-			>
-				<v-list-item
-					v-for="(value, name, index) in nav"
-					:key="index"
-					class="mr-11"
-					@click="goLink(value)"
-				>
-					<v-list-item-title v-text="name" class="vade-base-text" />
-				</v-list-item>
-				<v-list-item>
-					<v-btn
-						size="large"
-						variant="outlined"
-						class="text-lowercase vade-base-text"
-						:color="palette['primary-nav-text']"
-						>book a demo</v-btn
-					>
-				</v-list-item>
-			</v-list>
+			<vade-nav :nav="nav" :color="palette['primary-nav-text']" />
 		</template>
 	</v-app-bar>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
+import VadeNav from './VadeNav.vue';
 
-const router = useRouter();
 const store = useStore();
 
 const nav = {
@@ -52,10 +30,6 @@ const nav = {
 };
 
 const palette = store.state.palette;
-
-function goLink(path) {
-	return router.push(path);
-}
 </script>
 
 <style></style>
