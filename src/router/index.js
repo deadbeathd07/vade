71206f5f58d491f4c6e95a8c0dc35d53775cfe1a
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-07 18:15:04 +0300

    added pathes for other pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import TechnologyView from '../views/TechnologyView.vue';
+import AboutUsView from '../views/AboutUsView.vue';
+import ImpactView from '../views/ImpactView.vue';
+import PortfolioView from '../views/PortfolioView.vue';
+import BlogView from '../views/BlogView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +13,31 @@ const router = createRouter({
 			path: '/',
 			name: 'home',
 			component: HomeView,
+		},
+		{
+			path: '/technology',
+			name: 'technology',
+			component: TechnologyView,
+		},
+		{
+			path: '/about_us',
+			name: 'about_us',
+			component: AboutUsView,
+		},
+		{
+			path: '/impact',
+			name: 'impact',
+			component: ImpactView,
+		},
+		{
+			path: '/portfolio',
+			name: 'portfolio',
+			component: PortfolioView,
+		},
+		{
+			path: '/blog',
+			name: 'blog',
+			component: BlogView,
 		},
 	],
 });
