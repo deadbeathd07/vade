d6af1993f62505b983b8a45aed87d58f466033fb
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-07 12:42:40 +0300

    bug with state has been fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { createStore } from 'vuex';
 
 const store = createStore({
-	state: {},
+	state() {
+		return {
+			palette: {
+				'primary-nav-text': '#393737',
+			},
+		};
+	},
 	getters: {},
 	actions: {},
 	mutations: {},
