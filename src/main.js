7bf201ae1524c9b7f043f814a1e0fe879e488185
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-07 12:39:16 +0300

    store has been imported and switched on

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import './assets/index.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import vuetify from './vuetify';
 
 const app = createApp(App);
 
-app.use(router).use(vuetify);
+app.use(router).use(store).use(vuetify);
 
 app.mount('#app');
