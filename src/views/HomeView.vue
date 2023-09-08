ee5f251c9f991bcc297ccb8bff4ba3cebccf2d0d
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-08 16:13:37 +0300

    second page has been added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
-	<v-sheet class="pt-27 pb-25 bg-img-1">
+	<v-sheet class="bg-img-1">
 		<h1 class="vade-h1 text-regular vade-lh-115 vade-ls-696 mb-30">
 			Connecting the curb to new insights
 		</h1>
-		<p class="max-width-365 mb-9 primary-text vade-base-text vade-lh-24">
+		<p class="max-width-365 primary-text vade-base-text vade-lh-24">
 			Real-time curb data for smart cities. Measure occupancy, analyze demand,
 			and manage curb space with the right tools.
 		</p>
@@ -16,32 +16,73 @@
 			Book a demo
 		</v-btn>
 	</v-sheet>
+	<v-sheet class="vade-bg-primary bg-img-camera">
+		<h2>Get to know the curb better</h2>
+		<p class="max-width-45">
+			Vade replaces legacy parking infrastructure with wireless cameras,
+			computer vision, and granular analytics. We make it easy for cities to
+			monitor and manage curb space in real-time.
+		</p>
+		<v-btn
+			variant="outlined"
+			class="vade-base-text vade-lh-24 text-transform-none"
+			size="large"
+		>
+			More about our technology
+		</v-btn>
+		<v-sheet color="transparent" class="d-flex">
+			<v-card
+				v-for="(option, index) in options"
+				:key="index"
+				:title="option.title"
+				:text="option.text"
+				elevation="0"
+				color="transparent"
+			>
+				<template v-slot:prepend>
+					<component :is="option.img" />
+				</template>
+			</v-card>
+		</v-sheet>
+	</v-sheet>
 </template>
 
-<script setup></script>
+<script setup>
+import SolarBatteryIcon from '../components/icons/SolarBatteryIcon.vue';
+import DatabaseIcon from '../components/icons/DatabaseIcon.vue';
+import ChartIcon from '../components/icons/ChartIcon.vue';
+
+const options = [
+	{
+		img: SolarBatteryIcon,
+		title: 'Fully wireless hardware',
+		text: 'We provide solar and cellular cameras that work anywhere, operate 24/7, and take under 10 minutes to install.',
+	},
+	{
+		img: DatabaseIcon,
+		title: 'Real-time data',
+		text: 'Measure availability and compliance in real-time with our powerful and accurate computer vision pipeline.',
+	},
+	{
+		img: ChartIcon,
+		title: 'Full service subscription',
+		text: 'Full service subscription. One subscription for the cameras, installation, maintenance, implementation, software, and support.',
+	},
+];
+</script>
 
 <style>
-.pt-27 {
-	padding-top: 108px;
-}
-
-.pb-25 {
-	padding-bottom: 100px;
-}
-
-.mb-30 {
-	margin-bottom: 120px;
-}
-
-.py-20 {
-	padding-top: 20px;
-	padding-bottom: 20px;
-}
-
 .bg-img-1 {
 	background-image: url('../assets/bg-1.png') !important;
 	background-position: center !important;
 	background-size: cover !important;
+	background-repeat: no-repeat !important;
+}
+
+.bg-img-camera {
+	background-image: url('../assets/camera.png') !important;
+	background-position: right 126px !important;
+	background-size: 642px 414px !important;
 	background-repeat: no-repeat !important;
 }
 
@@ -55,5 +96,9 @@
 
 .max-width-365 {
 	max-width: 365px;
+}
+
+.max-width-450 {
+	max-width: 450px;
 }
 </style>
