b1287e059a6ddc1153cc5081890e562aaec9fc12
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-14 12:41:01 +0300

    footer component has been created

---
+++ src/components/VadeFooter.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-footer>
+		<v-sheet>
+			<vade-nav />
+			<address>
+				<p>team@vade.ai</p>
+				<v-list></v-list>
+			</address>
+		</v-sheet>
+		<v-divider />
+	</v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
