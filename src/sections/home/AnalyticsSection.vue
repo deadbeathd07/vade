af65dbbec3bfdb092a8fac8b29d972a30bb89a83
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-15 16:25:01 +0300

    stylization of the section typography was added according to the design

--- src/sections/home/AnalyticsSection.vue
+++ src/sections/home/AnalyticsSection.vue
@@ -1,13 +1,23 @@
 <template>
-	<v-sheet>
-		<h2>Collect, analyze, and visualize occupancy data</h2>
-		<p>Visualize demand with dashboards for curb utilization and compliance.</p>
-		<v-img src="assets/app.png" />
+	<v-sheet class="py-15 vade-px-26">
+		<h3
+			class="primary-title vade-h3 vade-lh-50 vade-ls-132 font-weight-regular vade-max-width-450 pb-6"
+		>
+			{{ sectionContent.title }}
+		</h3>
+		<p class="primary-text vade-base-text vade-lh-24 max-width-450 mb-15">
+			{{ sectionContent.text }}
+		</p>
+		<v-img :src="sectionContent.img_path" />
 	</v-sheet>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const sectionContent = store.state.sections.analytics;
 </script>
 
 <style></style>
