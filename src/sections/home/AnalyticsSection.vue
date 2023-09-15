a473b1cd19b2c61c7ff4cbbada4c440e09cc7422
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-15 16:53:41 +0300

    a background class for the analytics section was created, a spacer was created

--- src/sections/home/AnalyticsSection.vue
+++ src/sections/home/AnalyticsSection.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-sheet class="py-15 vade-px-26">
+	<v-sheet class="vade-bg-3 my-15 vade-px-26">
 		<h3
 			class="primary-title vade-h3 vade-lh-50 vade-ls-132 font-weight-regular vade-max-width-450 pb-6"
 		>
@@ -8,7 +8,7 @@
 		<p class="primary-text vade-base-text vade-lh-24 max-width-450 mb-15">
 			{{ sectionContent.text }}
 		</p>
-		<v-img :src="sectionContent.img_path" />
+		<v-sheet height="615" color="transparent" />
 	</v-sheet>
 </template>
 
@@ -20,4 +20,11 @@ const store = useStore();
 const sectionContent = store.state.sections.analytics;
 </script>
 
-<style></style>
+<style>
+.vade-bg-3 {
+	background-image: url('../../assets/app.png') !important;
+	background-position: right bottom !important;
+	background-size: auto !important;
+	background-repeat: no-repeat !important;
+}
+</style>
