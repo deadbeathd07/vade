2f6111537430f2c3b90eaf23c68bb04e5e63615c
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-15 14:07:27 +0300

    Fixed a bug with the reactivity of components in the repository when importing them

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import { createStore } from 'vuex';
+import SolarBatteryIcon from '../components/icons/SolarBatteryIcon.vue';
+import DatabaseIcon from '../components/icons/DatabaseIcon.vue';
+import ChartIcon from '../components/icons/ChartIcon.vue';
+import { shallowRef } from 'vue';
 
 const store = createStore({
 	state() {
@@ -13,6 +17,23 @@ const store = createStore({
 					title: 'Get to know the curb better',
 					text: 'Vade replaces legacy parking infrastructure with wireless cameras, computer vision, and granular analytics. We make it easy for cities to monitor and manage curb space in real-time.',
 					btn_text: 'More about our technology',
+					options: [
+						{
+							icon: shallowRef(SolarBatteryIcon),
+							title: 'Fully wireless hardware',
+							text: 'We provide solar and cellular cameras that work anywhere, operate 24/7, and take under 10 minutes to install.',
+						},
+						{
+							icon: shallowRef(DatabaseIcon),
+							title: 'Real-time data',
+							text: 'Measure availability and compliance in real-time with our powerful and accurate computer vision pipeline.',
+						},
+						{
+							icon: shallowRef(ChartIcon),
+							title: 'Full service subscription',
+							text: 'Full service subscription. One subscription for the cameras, installation, maintenance, implementation, software, and support.',
+						},
+					],
 				},
 			},
 			nav: {
