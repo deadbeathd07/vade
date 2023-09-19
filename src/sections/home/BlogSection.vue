fb8fe4e9113c1133c536f6cfb55317bad1e9ec03
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-19 19:53:59 +0300

    added visual design of posts and indents

--- src/sections/home/BlogSection.vue
+++ src/sections/home/BlogSection.vue
@@ -1,12 +1,29 @@
 <template>
-	<v-sheet>
-		<h5>{{ sectionContent.title }}</h5>
-		<v-card
-			v-for="(post, i) in sectionContent.posts"
-			:key="i"
-			:title="post.title"
-			:image="post.img"
-		></v-card>
+	<v-sheet class="vade-py-25 vade-px-26" color="#f9fbfd">
+		<h5 class="primary-title vade-h4 vade-lh-28 mb-6 font-weight-regular">
+			{{ sectionContent.title }}
+		</h5>
+		<v-container class="pa-0 vade-mb-22">
+			<v-row>
+				<v-col cols="4" v-for="(post, i) in sectionContent.posts" :key="i">
+					<v-card rounded="lg" color="white" elevation="0" class="pa-0 ma-0">
+						<v-img :height="273" cover :src="post.img" />
+						<div
+							class="height-160 d-flex flex-column justify-space-between pa-6"
+						>
+							<v-card-title
+								class="vade-h4 vade-lh-22 primary-title font-weight-regular text-wrap pa-0"
+							>
+								{{ post.title }}
+							</v-card-title>
+							<v-card-subtitle class="info-text vade-base-text vade-lh-24 pa-0">
+								{{ post.date }}
+							</v-card-subtitle>
+						</div>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
 		<v-btn>Read all posts</v-btn>
 	</v-sheet>
 </template>
@@ -19,4 +36,17 @@ const store = useStore();
 const sectionContent = store.state.sections.blog;
 </script>
 
-<style></style>
+<style>
+.vade-py-25 {
+	padding-top: 100px;
+	padding-bottom: 100px;
+}
+
+.height-160 {
+	height: 160px;
+}
+
+.vade-mb-22 {
+	margin-bottom: 88px;
+}
+</style>
