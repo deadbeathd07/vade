a5ae1a234d93863542154ede7e88a6e5f15d660d
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-19 22:58:28 +0300

    added typography design for layout

--- src/sections/home/FormSection.vue
+++ src/sections/home/FormSection.vue
@@ -1,10 +1,15 @@
 <template>
-	<v-sheet>
+	<v-sheet color="#ddebff" class="vade-py-27">
 		<v-container>
 			<v-row>
-				<v-col cols="6">
-					<h3>Let’s find out how your curbs are used.</h3>
-					<p>
+				<v-col
+					cols="6"
+					class="pt-12 pb-15 px-14 d-flex flex-column justify-space-between"
+				>
+					<h3 class="vade-h3 vade-lh-50 vade-ls-132 primary-title">
+						Let’s find out how your curbs are used.
+					</h3>
+					<p class="vade-h4 vade-lh-21 vade-ls-180">
 						Connect with Vade and see what real-time curb data can do for your
 						city.
 					</p>
@@ -24,8 +29,11 @@
 	</v-sheet>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup></script>
 
-<style></style>
+<style>
+.vade-py-27 {
+	padding-top: 108px;
+	padding-bottom: 108px;
+}
+</style>
