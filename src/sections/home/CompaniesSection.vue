81e54fe5dbed3d24f3212d621e6f7591c035dde2
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-19 17:17:30 +0300

    added visualization of the section according to the design layout

--- src/sections/home/CompaniesSection.vue
+++ src/sections/home/CompaniesSection.vue
@@ -1,27 +1,47 @@
 <template>
-	<v-sheet>
-		<h2>Trusted by leaders in urban transportation</h2>
-		<p>
+	<v-sheet class="vade-pt-31 vade-pb-45 vade-px-26">
+		<h3
+			class="primary-title vade-h3 vade-lh-50 vade-ls-132 font-weight-regular vade-max-width-450 pb-6"
+		>
+			Trusted by leaders in urban transportation
+		</h3>
+		<p class="primary-text vade-base-text vade-lh-24 max-width-450 vade-mb-24">
 			Transparency and quality are the basis of our work, and we've helped
 			cities and vendors across the United States.
 		</p>
-		<h6>Here's who we've worked with</h6>
-		<v-list color="transparent" class="d-flex flex-wrap">
-			<v-list-item v-for="(company, i) in companies" :key="i">
-				<template v-slot:prepend>
+		<h4 class="vade-h4 vade-lh-28 primary-title font-weight-regular mb-9">
+			Here's who we've worked with
+		</h4>
+		<v-container class="d-flex flex-wrap vade-px-18 vade-mb-41">
+			<v-row>
+				<v-col
+					cols="3"
+					v-for="(company, i) in companies"
+					:key="i"
+					class="pa-0 ma-0"
+				>
 					<component :is="company.icon" />
-				</template>
-			</v-list-item>
-		</v-list>
-		<figure>
-			<q>
+				</v-col>
+			</v-row>
+		</v-container>
+		<figure class="d-flex">
+			<q
+				class="original-text vade-quote-text vade-lh-46 vade-ls-32 flex-0-1 vade-mr-19 vade-max-width-600"
+			>
 				Curb management can help ensure that curb space is allocated more
 				equitably, providing access to this limited resource to all street
 				users, including our most vulnerable.
 			</q>
-			<figcaption>
-				<h6>San Francisco</h6>
-				Municipal Transportation Agency
+			<figcaption class="flex-0-0 d-flex align-center justify-center">
+				<company-logo-9 />
+				<div class="ml-5 vade-max-width-170">
+					<h6 class="vade-h4 primary-title vade-lh-28 font-weight-regular">
+						San Francisco
+					</h6>
+					<p class="vade-base-text vade-lh-24 primary-text">
+						Municipal Transportation Agency
+					</p>
+				</div>
 			</figcaption>
 		</figure>
 	</v-sheet>
@@ -36,6 +56,7 @@ import CompanyLogo5 from '../../components/icons/companies_logo/CompanyLogo5.vue
 import CompanyLogo6 from '../../components/icons/companies_logo/CompanyLogo6.vue';
 import CompanyLogo7 from '../../components/icons/companies_logo/CompanyLogo7.vue';
 import CompanyLogo8 from '../../components/icons/companies_logo/CompanyLogo8.vue';
+import CompanyLogo9 from '../../components/icons/companies_logo/companyLogo9.vue';
 
 const companies = [
 	{
@@ -65,4 +86,37 @@ const companies = [
 ];
 </script>
 
-<style></style>
+<style>
+.vade-pt-31 {
+	padding-top: 124px;
+}
+
+.vade-pb-45 {
+	padding-bottom: 180px;
+}
+
+.vade-mb-24 {
+	margin-bottom: 96px;
+}
+
+.vade-px-18 {
+	padding-left: 72px !important;
+	padding-right: 72px !important;
+}
+
+.vade-mb-41 {
+	margin-bottom: 164px;
+}
+
+.vade-mr-19 {
+	margin-right: 76px;
+}
+
+.vade-max-width-600 {
+	max-width: 600px;
+}
+
+.vade-max-width-170 {
+	max-width: 170px;
+}
+</style>
