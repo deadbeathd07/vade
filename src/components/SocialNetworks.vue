f1fb9ae68f4db0a847649ab225ed322fce4feb0d
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 20:09:23 +0300

    social networks component has been finished

---
+++ src/components/SocialNetworks.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-list lines="one" class="d-flex">
+		<v-list-item v-for="(item, i) in props.list" :key="i" class="pa-0 mr-3">
+			<a href="#">
+				<v-icon
+					:icon="'mdi-' + item"
+					size="x-large"
+					color="black"
+					class="vade-social-icon"
+				/>
+			</a>
+		</v-list-item>
+	</v-list>
+</template>
+
+<script setup>
+const props = defineProps({
+	list: Array,
+});
+</script>
+
+<style>
+.vade-social-icon:hover {
+	color: var(--palette-1-5) !important;
+	transition: all 0.3s ease-in;
+}
+</style>
