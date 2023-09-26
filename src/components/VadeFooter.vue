86a7949789a13af726a050f504aac8461e6ca7ca
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 20:27:43 +0300

    footer finished

--- src/components/VadeFooter.vue
+++ src/components/VadeFooter.vue
@@ -12,7 +12,7 @@
 				:is-horizontal="false"
 				class="ml-auto flex-0-1 vade-mr-12-pc"
 			/>
-			<address class="flex-0-1">
+			<address class="flex-0-1 vade-mr-9-pc">
 				<a
 					href="#"
 					target="_blank"
@@ -22,9 +22,20 @@
 					team@vade.ai
 				</a>
 				<social-networks :list="socialNetworksList" />
+				<p
+					class="font-normal primary-text vade-base-text vade-lh-24 vade-max-width-175"
+				>
+					16 W Martin St. Suite 1101, Raleigh, NC 27601
+				</p>
 			</address>
 		</v-sheet>
-		<v-divider />
+		<p class="w-100 vade-info-text info-text mb-4">© 2021 VADE Group Inc.</p>
+		<v-divider class="w-100" />
+		<v-list class="w-100 d-flex pa-0 ma-0 vade-info-text">
+			<v-list-item v-for="(item, i) in termsOfUse" :key="i" class="pa-0 mr-3">
+				<a href="#" class="text-decoration-none info-text">{{ item }}</a>
+			</v-list-item>
+		</v-list>
 	</v-footer>
 </template>
 
@@ -38,6 +49,7 @@ const store = useStore();
 const nav = store.state.nav;
 
 const socialNetworksList = ['facebook', 'instagram', 'twitter', 'linkedin'];
+const termsOfUse = ['privacy policy', 'terms of use'];
 </script>
 
 <style>
@@ -46,5 +58,11 @@ const socialNetworksList = ['facebook', 'instagram', 'twitter', 'linkedin'];
 }
 .vade-mr-12-pc {
 	margin-right: 12%;
+}
+.vade-mr-9-pc {
+	margin-right: 9%;
+}
+.vade-max-width-175 {
+	max-width: 175px;
 }
 </style>
