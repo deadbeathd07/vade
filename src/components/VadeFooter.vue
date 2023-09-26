062635378397a3a1623b2082726d496affc6ecea
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 19:31:10 +0300

    added logo, navigation and margins between components

--- src/components/VadeFooter.vue
+++ src/components/VadeFooter.vue
@@ -1,9 +1,26 @@
 <template>
-	<v-footer class="d-flex flex-column">
-		<v-sheet class="w-100 d-flex justify-space-between">
-			<vade-nav :nav="nav" :is-horizontal="false" />
-			<address>
-				<p>team@vade.ai</p>
+	<v-footer class="d-flex flex-column pa-0 pt-13 pb-5">
+		<v-sheet class="w-100 d-flex">
+			<v-img
+				:width="44"
+				:height="48"
+				src="assets/logo-2.png"
+				class="flex-0-0"
+			/>
+			<vade-nav
+				:nav="nav"
+				:is-horizontal="false"
+				class="ml-auto flex-0-1 vade-mr-12-pc"
+			/>
+			<address class="flex-0-1">
+				<a
+					href="#"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="font-normal text-decoration-none primary-nav-text vade-base-text vade-lh-18"
+				>
+					team@vade.ai
+				</a>
 			</address>
 		</v-sheet>
 		<v-divider />
@@ -19,4 +36,11 @@ const store = useStore();
 const nav = store.state.nav;
 </script>
 
-<style></style>
+<style>
+.font-normal {
+	font-style: normal;
+}
+.vade-mr-12-pc {
+	margin-right: 12%;
+}
+</style>
